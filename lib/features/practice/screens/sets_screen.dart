42bf66d04d326d84cb8cbb5907a89edfb9577be0
opkukna1 <Opsiddh41@gmail.com>
2025-09-16 20:23:23 +0530

    Update sets_screen.dart

--- lib/features/practice/screens/sets_screen.dart
+++ lib/features/practice/screens/sets_screen.dart
@@ -1,3 +1,5 @@
+// lib/features/practice/screens/sets_screen.dart
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import '../../../core/local_data_service.dart';
@@ -8,7 +10,6 @@ class SetsScreen extends StatelessWidget {
 
   SetsScreen({super.key, required this.subject, required this.topic});
   
-  // डेटा मैनेजर का कनेक्शन जोड़ें
   final LocalDataService localDataService = LocalDataService();
 
   @override
@@ -17,17 +18,28 @@ class SetsScreen extends StatelessWidget {
     final numberOfSets = (questions.length / 10).ceil();
 
     return Scaffold(
+      backgroundColor: Colors.blueGrey[50], // नया बैकग्राउंड कलर
       appBar: AppBar(
         title: Text(topic),
+        backgroundColor: Colors.white,
+        elevation: 1,
       ),
       body: ListView.builder(
+        padding: const EdgeInsets.all(12.0),
         itemCount: numberOfSets,
         itemBuilder: (context, index) {
           final setIndex = index + 1;
           return Card(
+            elevation: 2,
+            margin: const EdgeInsets.symmetric(vertical: 6.0),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
             child: ListTile(
-              title: Text('Set $setIndex'),
-              trailing: const Icon(Icons.arrow_forward_ios),
+              leading: CircleAvatar(
+                backgroundColor: Colors.orange.withOpacity(0.1),
+                child: const Icon(Icons.view_list_outlined, color: Colors.orange),
+              ),
+              title: Text('Set $setIndex', style: const TextStyle(fontWeight: FontWeight.w500)),
+              trailing: const Icon(Icons.arrow_forward_ios_rounded, size: 18),
               onTap: () {
                 context.go(
                   '/practice-mcq',
